e80c638562f10aeb8d45df0e18a5621c4a640b30
elbekdavron <elbekd4vronov@gmail.com>
2025-01-14 16:28:26 +0500

    09

---
+++ sendAudio.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+def send_audio(chat_id:str, audio:str):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
+    param = {
+        'chat_id':chat_id
+    }
+    files = {
+        'audio':audio
+    }
+    response = requests.post(url,params=param,files=files)
+    return response.json
+mp3 = open('audio.mp3', 'rb').read()
+chat_id = 988528730
+send_audio(chat_id=chat_id, audio=mp3)
